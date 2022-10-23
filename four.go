f58a7e4a7fbdf3fc85b7a5a647af1b08e5b9ec70
Josh Deprez <josh.deprez@gmail.com>
2022-10-23 15:02:06 +1100

    four: use log.Fatalf instead of panic

--- four.go
+++ four.go
@@ -1,10 +1,12 @@
-// The four binary randomly chooses four words from /usr/share/dict/words.
+// The `four` command randomly chooses four short words from
+// `/usr/share/dict/words`.
 package main
 
 import (
 	"bufio"
 	"crypto/rand"
 	"fmt"
+	"log"
 	"math/big"
 	"os"
 )
@@ -14,22 +16,26 @@ const wordsFile = "/usr/share/dict/words"
 func main() {
 	f, err := os.Open(wordsFile)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Couldn't open words file: %v", err)
 	}
 	defer f.Close()
 	var words []string
 	sc := bufio.NewScanner(f)
 	for sc.Scan() {
-		words = append(words, sc.Text())
+		w := sc.Text()
+		if len(w) > 6 {
+			continue
+		}
+		words = append(words, w)
 	}
 	if err := sc.Err(); err != nil {
-		panic(err)
+		log.Fatalf("Couldn't scan words file: %v", err)
 	}
 	l := big.NewInt(int64(len(words)))
 	for i := 0; i < 4; i++ {
 		r, err := rand.Int(rand.Reader, l)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Couldn't read random source: %v", err)
 		}
 		fmt.Printf("%s", words[int(r.Int64())])
 	}
